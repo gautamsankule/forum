d67ebc753b5700234b185e07cc72110d3f4a40d2
gautamsankule <gautamsankule500@gmail.com>
2018-02-20 18:49:22 +0530

    Basic styles and structures

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
 before_action :find_post, only: [:show,:edit,:update,:destroy]
 def index
-	@post = Post.all.order("created_at DESC")
+	@posts = Post.all.order("created_at DESC")
   end 
   def show
        
    end
   def new
-    @post = current_user.post.build
+    @post = current_user.posts.build  #Post.new
   end 
   def create
-  	@post = current_user.post.build(post_params)
+  	@post = current_user.posts.build(post_params)  #Post.new(post_params)
   	if @post.save 
    		redirect_to @post
   	else  
